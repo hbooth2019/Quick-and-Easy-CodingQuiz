07189984a9914692c35466161ecf2809504e69c7
Hbooth2019 <boothhunter2@gmail.com>
2020-06-30 18:23:10 -0600

    First Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+var startBtn = document.querySelector("#start-button")
+var timeEl = document.querySelector("#timer")
+
+var myQuestions = [
+{
+
+ question: "what is a boolean,"
+ answers: 
+}]
+
+function renderQuestion(){
+    document.querySelector(".questions").innerHTML = "";
+    startQuiz.setAttribute("style"), display: none");
+    document.querySelector(".quiz").setAttribute("style")
+    var q = questions[j].questions
+
+
+
+    for (var)
+}
+function counter (){
+
+}
+function checkAnswer(){
+    console.log("check");
+    if (questions[j].correct !==event.target.textContent){
+    }
+    if (j < questions.length)
+}
+function endGame(){
+    clearInterval;
+    document.querySelector(".results")setAttribute("style,)
+
+}
+function storeScores(){
+
+}
+
+startQuiz.addEventListener("click,renderQuestion")
