e4381a2d48d9c0395a4258a6a3d6b0f1da508086
Hbooth2019 <boothhunter2@gmail.com>
2020-06-30 18:34:52 -0600

    Added my questions in there!

--- script.js
+++ script.js
@@ -3,33 +3,77 @@ var timeEl = document.querySelector("#timer")
 
 var myQuestions = [
 {
+    question: "Question,"
+    answers: ["answer1", "answer 2","answer 3","answer 4",]
+    correctAns: "answer1"
+   }
+   {
+    question: "Question,"
+    answers: ["answer1", "answer 2","answer 3","answer 4",]
+    correctAns: "answer1"
+   }
+   {
+    question: "Question,"
+    answers: ["answer1", "answer 2","answer 3","answer 4",]
+    correctAns: "answer1"
+   }
+   {
+    question: "Question,"
+    answers: ["answer1", "answer 2","answer 3","answer 4",]
+    correctAns: "answer1"
+    question: "Question,"
+    answers: ["answer1", "answer 2","answer 3","answer 4",]
+    correctAns: "answer1"
+   }
+   {
+    question: "Question,"
+    answers: ["answer1", "answer 2","answer 3","answer 4",]
+    correctAns: "answer1"
+   }
+   {
+    question: "Question,"
+    answers: ["answer1", "answer 2","answer 3","answer 4",]
+    correctAns: "answer1"
+   }
+   {
+    question: "Question,"
+    answers: ["answer1", "answer 2","answer 3","answer 4",]
+    correctAns: "answer1"
+ } 
 
- question: "what is a boolean,"
- answers: 
-}]
+
+ ]
 
 function renderQuestion(){
     document.querySelector(".questions").innerHTML = "";
     startQuiz.setAttribute("style"), display: none");
-    document.querySelector(".quiz").setAttribute("style")
-    var q = questions[j].questions
+    document.querySelector(".quiz").setAttribute("style");
+    var q = questions[j].questions;
+    var questionEl = document.createElement("h2");
 
 
 
     for (var)
 }
 function counter (){
+    interval = setInterval(function() => {
+       document.querySelector("#timer")
+       secondsLeft--;
+       if(secondsleft === 0) {
+       clearInterval;
+       }
+    }, interval);
 
 }
 function checkAnswer(){
     console.log("check");
     if (questions[j].correct !==event.target.textContent){
     }
-    if (j < questions.length)
+    if (j < questions.length) {j++};
 }
 function endGame(){
     clearInterval;
-    document.querySelector(".results")setAttribute("style,)
+    document.querySelector(".results")setAttribute("style", "display:block");
 
 }
 function storeScores(){
